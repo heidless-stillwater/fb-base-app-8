e6cba7e7daece12a5807c646c022df0580d2b45e
rob <heidlessemail17@gmail.com>
2025-11-19 16:09:43 +0000

    File changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { Loader2 } from 'lucide-react';
+
 import FileManager from '@/components/file-manager';
+import { useUser } from '@/firebase';
 
 export default function Home() {
+  const { user, isUserLoading } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isUserLoading && !user) {
+      router.push('/auth');
+    }
+  }, [user, isUserLoading, router]);
+
+  if (isUserLoading || !user) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Loader2 className="h-16 w-16 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <main className="flex items-start justify-center min-h-screen p-4 sm:p-6 md:p-8">
       <div className="w-full max-w-7xl">
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "heidless-firebase-0",
-  "appId": "1:540584376441:web:54ae9a1812fdd943627ca8",
-  "apiKey": "AIzaSyD5l4EEekFk3gx00D0_AB66MOMonaiglho",
-  "authDomain": "heidless-firebase-0.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "540584376441"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
